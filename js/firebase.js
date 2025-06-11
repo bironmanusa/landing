c3f04ea78bf13f232e5057560ae709816f4794dc
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2025-06-11 19:02:44 +0000

    .env

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCMovWU_E9pkyvZtwpKenoOsEljGr6sxeM",
+    authDomain: "prueba-94d59.firebaseapp.com",
+    projectId: "prueba-94d59",
+    storageBucket: "prueba-94d59.firebasestorage.app",
+    messagingSenderId: "784675813944",
+    appId: "1:784675813944:web:e8b15d32078dea37f784cd",
+    measurementId: "G-S70S4C3G1K"
+  };
